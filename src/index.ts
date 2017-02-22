2c794368198c910aec2fbaf925312bd8cae4c3d5
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2017-02-22 10:21:42 +0900

    add animation

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,55 @@ import Feed from './model/feed'
 import Conditions from './model/conditions'
 import search from './service/searchService'
 
-const feed = new Feed()
-
-/**
- * function render(json) {
- * }
- * と同じ
- */
-const render = (json:any) => {
-    console.log(json)
-    let element:Element = document.querySelector('.container .row')
-
-    for(let i = 0; i < json.response.docs.length; i++) {
-        element.appendChild(feed.getFeed(json.response.docs[i]))
-    }
-};
-
 window.onload = () => {
-    document.querySelector(".btn-outline-success").addEventListener('click' , () => {
-            document.querySelector('.container .row').innerHTML = ''
-            let conditions = new Conditions((<HTMLInputElement>document.querySelector('.form-control')).value)
-            search(conditions)
-                .then(render)
-                .catch(window.alert) 
+
+    const feed = new Feed()
+    const target:Element = document.querySelector('.container .row')
+    const loading:Element = document.querySelector('.loading')
+
+    /**
+     * function render(json) {
+     * }
+     * と同じ
+     */
+    const render = (json:any) => {
+        try {
+            console.log(json)
+            target.innerHTML = ''
+
+            for(let i = 0; i < json.response.docs.length; i++) {
+                target.appendChild(feed.getFeed(json.response.docs[i]))
+            }
+        } catch(ex) {
+            console.log(ex)
+        }
+    }
+
+    const sleep = (time:Number) => {
+        const d1:any = new Date()
+        while (true) {
+            const d2:any = new Date()
+            if (d2 - d1 > time) {
+                break
+            }
+        }
+    }
+
+    document.querySelector(".btn-outline-success").addEventListener('click', () => {
+            /** try-catch書けます **/
+            try {
+                target.innerHTML = ''
+                console.log(loading)
+                target.appendChild(loading.cloneNode(true))
+                sleep(500)
+
+                let conditions = new Conditions((<HTMLInputElement>document.querySelector('.form-control')).value)
+                search(conditions)
+                    .then(render)
+                    .catch(window.alert)
+            } catch(ex) {
+                console.log(ex)
+            }
         }
     )
 }
--- src/service/searchService.ts
+++ src/service/searchService.ts
@@ -3,23 +3,23 @@
 import Searcher from '../interface/searcher';
 import Conditions from '../model/conditions';
 
-const url:string = 'http://kn.ndl.go.jp/api/search?searchPattern=simple';
-
 /** interfaceの実装 **/
 const search: Searcher<Conditions> = (condition) => {
-    return new Promise((resolve, reject) => {
-        let target = url + '&keyword=' + encodeURIComponent(condition.keyword)
-        console.log(target)
-        
-        fetch(target)
-            .then(function(response) {
-                return response.json()
-            }).then(function(json) {
-                resolve(json)
-            }).catch(function(ex) {
-                reject('search ng!!!')
-            })
-    });  
-};
+    try {
+        return new Promise((resolve, reject) => {        
+            window.fetch('http://kn.ndl.go.jp/api/search?searchPattern=simple' + '&keyword=' + encodeURIComponent(condition.keyword))
+                .then(function(response) {
+                    return response.json()
+                }).then(function(json) {
+                    console.log(json)
+                    resolve(json)
+                }).catch(function(ex) {
+                    reject('search ng!!!')
+                })
+        })
+    } catch (ex) {
+        console.log(ex)
+    }
+}
 
 export default search;
